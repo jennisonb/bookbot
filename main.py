b7a0b9fdcb39d720237683add69864707a67c2b7
jennisonb <jennisonb@gmail.com>
2025-03-20 22:03:31 -0600

    Final changes to complete the project

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import get_number_of_words
+import sys
+from stats import *
 
 def main():
-    path_to_file = "books/frankenstein.txt"
+    num_args = len(sys.argv)
+    if num_args < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
     book_contents = get_book_text(path_to_file)
     num_words = get_number_of_words(book_contents)
-    print(f"{num_words} words found in the document")
+    the_dict = get_number_of_each_character(book_contents)
+    print_report(book_contents)
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,2 +1,28 @@
+from curses.ascii import isalpha
+
+
 def get_number_of_words(book_contents):
     return len(book_contents.split())
+
+def get_number_of_each_character(book_contents):
+    letters_obj = {}
+    for letter in book_contents:
+        cur_letter = letter.lower()
+        if cur_letter not in letters_obj:
+            letters_obj[cur_letter] = 0
+        letters_obj[letter.lower()] +=1
+    return letters_obj
+
+def print_report(book_contents):
+    num_words = get_number_of_words(book_contents)
+    letters_dict = get_number_of_each_character(book_contents)
+    sorted_dict = sorted(letters_dict.items(), key=lambda x: x[1], reverse=True)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for k, v in sorted_dict:
+        if isalpha(k) == True:
+            print(f"{k}: {v}")
+    pass
